d35e1f1c7109061fd54e199d1869542619f4ebf0
ddominguez-27 <ddominguez27@gcds.net>
2025-10-15 13:52:45 -0400

    commente

--- cs2/ddominguez_postoffice.py
+++ cs2/ddominguez_postoffice.py
@@ -17,13 +17,13 @@ def size_tracker(l, h, w):
         size_tracked = 0
     return(size_tracked)
 
-
+#attempts data conversion and if not returns empty values
 def data_conversion(l, h, w, zip1, zip2):
     try:
         return(float(l), float(w), float(h), int(zip1), int(zip2))
     except Exception:
         return 0, 0, 0, 0, 0
-    
+#returns a zone value from a zip value
 def zone_counter(zip):
     if 1 <= zip <= 6999:
         return 1
@@ -39,11 +39,15 @@ def zone_counter(zip):
         return 6
     else:
         return 
+    
+#takes in dimensions and zone different and returns a monetary value
 def money(dm, zd):
     parcel_price = [0, .2, .37, .37, .6, 2.95, 3.95]
     zone_price = [0, .03, .03, .04, .05, .25, .35]
     price = parcel_price[dm] + (zone_price[dm]*zd)
     return price
+
+#main function
 def main():
     while True:
         data = input("please insert data (Length, Height, Width, Origin_Zipcode, Destiation_Zipcode)")
@@ -57,22 +61,34 @@ def main():
                 pass
             else:
                 data_list[data_list_counter] += i
-        
+        #removes any other zeroes
         for i in range(len(data_list)):
             data_list[i] = data_list[i].strip()
+        
+        #converts the variables into their proper data classes
         l, h, w, zip1, zip2, = data_conversion(data_list[0], data_list[1], data_list[2], data_list[3], data_list[4])
+
+        #checks to see if the try accept failed
         if ((l or h or w or zip1 or zip2) == 0) or data_list[5]:
             print("Input does not conform to possible values")
             continue
 
+        #converts zip codes into zones
         zone1 = zone_counter(zip1)
         zone2 = zone_counter(zip2)
-        zone_dif = abs(zone1 - zone2)
 
-        correct_money = str(money(size_tracker(l, w, h), zone_dif))
+        #variable that is prepping to format correctly- 
+        #size trakcer takes the dimensions and returns a value representing the parcel size
+        #takes the positive difference of the zones
+        #money function takes the 2 values and turns it into a value
+        #then turns it into a float
+        correct_money = float(money(size_tracker(l, w, h), abs(zone1 - zone2)))
 
-        
-        correct_money = correct_money.lstrip('0')
+        #formats the float with 2 decimal points
+        correct_money = f"{correct_money:.2f}"
+        #removes the first zero
+        correct_money = correct_money.lstrip("0")
+
         print(correct_money)
         
 
