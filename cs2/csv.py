ff13831508eb7088382331a0379040b40450861b
ddominguez-27 <ddominguez27@gcds.net>
2025-11-17 11:36:57 -0500

    changes

---
+++ cs2/csv.py
@@ -0,0 +1,11 @@
+try:
+    with open('titanic.csv', 'r') as file:
+        #header = file.readline().strip().split(',')  # Read the header row
+        #name_index = header.index('Name')  # Find the index of 'Name' column
+       
+        for line in file:
+            row = line.strip().split(',')
+            print(row)
+            #print(row[name_index])
+except FileNotFoundError:
+    print("Error: 'titanic.csv' file not found.")
