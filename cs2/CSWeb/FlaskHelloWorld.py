34bded98b6156078ffdc90e3db8e9442df78cda0
ddominguez-27 <ddominguez27@gcds.net>
2026-05-28 14:54:38 -0400

    commit

--- cs2/CSWeb/FlaskHelloWorld.py
+++ cs2/CSWeb/FlaskHelloWorld.py
@@ -7,10 +7,8 @@ app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
-    #print(app.url_map)
-    #print('line 17')            # Print statements go to your console
+
     
-    #return render_template("index.html")
     return render_template("template1.html")  # Return this html  as a response to the client
 
 @app.route('/submit', methods=["GET"])
@@ -23,11 +21,11 @@ def submit():
     with open("responses.csv", mode="a", newline="") as file:       #writes to the responses csv using append mode, so the file can accumulate data
         writer = csv.writer(file)
 
-        # write one row
-        writer.writerow([q1, q2, q3, q4])    
+        writer.writerow([q1, q2, q3, q4])        # write one row
 
 
-    return f"Responses submitted!"
+
+    return f"Responses submitted!"     #updates the page to confirm submission of response
 
 
 
