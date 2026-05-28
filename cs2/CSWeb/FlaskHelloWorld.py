ddd823bd6b5989bbfd4e558153ea3c20d75c2c4d
ddominguez-27 <ddominguez27@gcds.net>
2026-05-28 14:47:31 -0400

    commit

--- cs2/CSWeb/FlaskHelloWorld.py
+++ cs2/CSWeb/FlaskHelloWorld.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request  # From module flask import class Flask
+from flask import Flask, render_template, request  # imports the necesary 
 import csv
 
 
@@ -15,12 +15,20 @@ def main():
 
 @app.route('/submit', methods=["GET"])
 def submit():
-    q1 = request.args.get("q1")
+    q1 = request.args.get("q1")         #reads the answer for all 4 questions and assigns them to variables q1-4
     q2 = request.args.get("q2")
     q3 = request.args.get("q3")
     q4 = request.args.get("q4")
 
-    return f"{q1}, {q2}, {q3}, {q4}"
+    with open("responses.csv", mode="a", newline="") as file:       #writes to the responses csv using append mode, so the file can accumulate data
+        writer = csv.writer(file)
+
+        # write one row
+        writer.writerow([q1, q2, q3, q4])    
+
+
+    return f"Responses submitted!"
+
 
 
 
